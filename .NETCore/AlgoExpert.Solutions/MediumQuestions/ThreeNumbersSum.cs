f81032b6815f3d9db0371de630c10ef4d72f5fc8
thanhdoan <thanh.doan1@rbi.co.uk>
2019-12-17 15:47:10 +0000

    Thanh | More array questions

---
+++ .NETCore/AlgoExpert.Solutions/MediumQuestions/ThreeNumbersSum.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace AlgoExpert.Solutions.MediumQuestions
+{
+    public class ThreeNumbersSum
+    {
+        // Time O(n^3) | Space O(n)
+        public static List<int[]> ThreeNumbersSumSolutionOne(int[] arr, int target)
+        {
+            Array.Sort(arr);
+            var tripples = new List<int[]>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int m = j + 1; m < arr.Length; m++)
+                    {
+                        var sum = arr[i] + arr[j] + arr[m];
+                        if (sum == target)
+                        {
+                            var tripple = new int[3];
+                            tripple[0] = arr[i];
+                            tripple[1] = arr[j];
+                            tripple[2] = arr[m];
+                            Array.Sort(tripple);
+                            tripples.Add(tripple);
+                        }
+                    }
+                }
+            }
+            return tripples;
+        }
+
+        // O(^2) time | O(n) Space
+        public static List<int[]> ThreeNumbersSumSolutionTwo(int[] arr, int targetSum)
+        {
+            Array.Sort(arr);
+            var tripples = new List<int[]>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var left = i + 1;
+                var right = arr.Length - 1;
+                while (left < right)
+                {
+                    var checkSum = arr[i] + arr[left] + arr[right];
+
+                    if (checkSum == targetSum)
+                    {
+                        var tripple = new int[3];
+                        tripple[0] = arr[i];
+                        tripple[1] = arr[left];
+                        tripple[2] = arr[right];
+                        Array.Sort(tripple);
+                        tripples.Add(tripple);
+                    }
+
+                    if (checkSum < targetSum)
+                    {
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
+
+            return tripples;
+        }
+
+        public static int TwoPointerSolutionOne(int[] arr, int targetSum)
+        {
+            var sum = 0;
+            var count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+            }
+            return count;
+        }
+    }
+}
---
+++ .NETCore/AlgoExpert.Tests/MediumQuestions/ThreeNumbersSumTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using AlgoExpert.Solutions.MediumQuestions;
+using FluentAssertions;
+using Xunit;
+
+namespace AlgoExpert.Tests.MediumQuestions
+{
+    public class ThreeNumbersSumTest
+    {
+        [Fact]
+        public void ShouldReturnExpectedThreeNumbersSumToTargetNumber()
+        {
+            var arr = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
+            var targetNumber = 0;
+
+            var result = ThreeNumbersSum.ThreeNumbersSumSolutionOne(arr, targetNumber);
+
+            result.Count.Should().Be(3);
+        }
+
+        [Fact]
+        public void ShouldReturnEmpty2DArraySumToTargetNumber()
+        {
+            var arr = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
+            var targetNumber = 45;
+
+            var result = ThreeNumbersSum.ThreeNumbersSumSolutionOne(arr, targetNumber);
+
+            result.Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void ShouldReturnExpectedThreeNumbersSumToTargetNumberSolutionTwo()
+        {
+            var arr = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
+            var targetNumber = 0;
+
+            var result = ThreeNumbersSum.ThreeNumbersSumSolutionTwo(arr, targetNumber);
+
+            result.Count.Should().Be(3);
+            result[0][0].Should().Be(-8);
+            result[0][1].Should().Be(2);
+            result[0][2].Should().Be(6);
+        }
+
+        [Fact]
+        public void ShouldReturnEmpty2DArraySumToTargetNumberSolutionTwo()
+        {
+            var arr = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
+            var targetNumber = 45;
+
+            var result = ThreeNumbersSum.ThreeNumbersSumSolutionTwo(arr, targetNumber);
+
+            result.Count.Should().Be(0);
+        }
+
+
+        [Fact]
+        public void ShouldReturnEmpty2DArraySumToTargetTwoPoints()
+        {
+            var arr = new int[] { 1, 2, 5, 4, 2 };
+            var targetNumber = 8;
+
+            var result = ThreeNumbersSum.TwoPointerSolutionOne(arr, targetNumber);
+
+            result.Should().Be(0);
+        }
+    }
+}
